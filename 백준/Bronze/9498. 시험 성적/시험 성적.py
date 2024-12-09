314090815dce317c8222cceb01e4d06afb392054
Jian Yoo <soiseaubleu@gmail.com>
2024-12-09 17:46:21 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+G = int(input())
+
+if G >= 90:
+    print("A")
+elif 80 <= G <= 89:
+    print("B")
+elif 70 <= G <= 79:
+    print("C")
+elif 60 <= G <= 69:
+    print("D")
+else:
+    print("F")
